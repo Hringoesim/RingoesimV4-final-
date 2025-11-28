e533a6609fd57374e8b0c2267eb938c908981563
Hringoesim <hippolyte@ringoesim.com>
2025-11-28 18:32:25 +0100

    Fix NO_FCP: Eager load Index page

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Suspense, lazy } from "react";
 
-const Index = lazy(() => import("./pages/Index"));
+import Index from "./pages/Index";
 const HowItWorks = lazy(() => import("./pages/HowItWorks"));
 const Pricing = lazy(() => import("./pages/Pricing"));
 const UseCases = lazy(() => import("./pages/UseCases"));
